b101a7b7e53e5cae7c87d889787894b75d9b6e82
Łukasz Piotr Staniszewski <l.staniszewski@stud.elka.pw.edu.pl>
2021-04-15 03:13:08 +0200

    full task2?

--- test_main.py
+++ test_main.py
@@ -42,10 +42,12 @@ def test_methods():
 
 
 def test_auth():
-    response = client.get("/auth/?password=haslo&password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215")
+    response = client.get(
+        "/auth/?password=haslo&password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215")
     assert response.status_code == 204
 
-    response = client.get("/auth/?password=haslo&password_hash=f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091")
+    response = client.get(
+        "/auth/?password=haslo&password_hash=f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091")
     assert response.status_code == 401
 
     response = client.get("/auth/?password&password_hash")
@@ -54,36 +56,46 @@ def test_auth():
 
 def test_register():
     date_today = date.today()
-    response = client.post("/register/", json={"name": "Lukasz", "surname": "Staniszewski"})
+    response = client.post(
+        "/register/", json={"name": "Lukasz", "surname": "Staniszewski"})
     assert response.status_code == 201
     date_then = date_today + timedelta(days=18)
-    assert response.json() == {"id": 1, "name": "Lukasz", "surname": "Staniszewski", "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
+    assert response.json() == {"id": 1, "name": "Lukasz", "surname": "Staniszewski",
+                               "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
 
-    response = client.post("/register/", json={"name": "Marcin", "surname": "Najman"})
+    response = client.post(
+        "/register/", json={"name": "Marcin", "surname": "Najman"})
     assert response.status_code == 201
     date_then = date_today + timedelta(days=12)
-    assert response.json() == {"id": 2, "name": "Marcin", "surname": "Najman", "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
+    assert response.json() == {"id": 2, "name": "Marcin", "surname": "Najman",
+                               "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
 
-    response = client.post("/register/", json={"name": "Krzysztof", "surname": "Kolumb"})
+    response = client.post(
+        "/register/", json={"name": "Krzysztof", "surname": "Kolumb"})
     assert response.status_code == 201
     date_then = date_today + timedelta(days=15)
-    assert response.json() == {"id": 3, "name": "Krzysztof", "surname": "Kolumb", "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
+    assert response.json() == {"id": 3, "name": "Krzysztof", "surname": "Kolumb",
+                               "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
 
-    response = client.post("/register/", json={"name": "Krzysztof", "surname": "Kolumb"})
+    response = client.post(
+        "/register/", json={"name": "Krzysztof", "surname": "Kolumb"})
     assert response.status_code == 201
     date_then = date_today + timedelta(days=15)
-    assert response.json() != {"id": 5, "name": "Krzysztof", "surname": "Kolumb", "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
+    assert response.json() != {"id": 5, "name": "Krzysztof", "surname": "Kolumb",
+                               "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
 
 
 def test_patient():
     date_today = date.today()
-    response_post = client.post("/register/", json={"name": "Lukasz", "surname": "Staniszewski"})
+    response_post = client.post(
+        "/register/", json={"name": "Lukasz", "surname": "Staniszewski"})
     assert response_post.status_code == 201
     date_then = date_today + timedelta(days=18)
 
     response_get = client.get("/patient/1")
     assert response_get.status_code == 200
-    assert response_get.json() == {"id": 1, "name": "Lukasz", "surname": "Staniszewski", "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
+    assert response_get.json() == {"id": 1, "name": "Lukasz", "surname": "Staniszewski",
+                                   "register_date": f"{date_today}", "vaccination_date": f"{date_then}"}
 
     response_get2 = client.get("/patient/8")
     assert response_get2.status_code == 404
