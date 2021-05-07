57a8421559273da34de4f324062402ba9b2ab9df
Łukasz Piotr Staniszewski <l.staniszewski@stud.elka.pw.edu.pl>
2021-05-07 13:12:04 +0200

    rest of tasks

--- dbrouter.py
+++ dbrouter.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Response, status
+from fastapi import APIRouter, Request, Response, status, HTTPException, Query
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import sqlite3
@@ -10,6 +10,13 @@ class Customer(BaseModel):
 
 class Shipper(BaseModel):
     company_name: str
+
+class CategoryInput(BaseModel):
+    name: str
+
+class CategoryOutput(BaseModel):
+    id: int
+    name: str
 
 dbrouter = APIRouter()
 dbrouter.__name__ = "DataBase app!"
@@ -176,3 +183,129 @@ async def get_customers(response: Response):
     return {
         "customers": customers,
     }
+
+
+@dbrouter.get("/products/{product_id}")
+async def get_product_by_id(response: Response, product_id: int):
+    dbrouter.db_connection.row_factory = sqlite3.Row
+    data = dbrouter.db_connection.execute(
+        f"SELECT ProductID, ProductName FROM Products WHERE ProductID = {product_id}").fetchone()
+    if data is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There is no product with such id"
+        )
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {"id": data["ProductID"], "name": data["ProductName"]}
+
+
+@dbrouter.get("/employees")
+async def get_employees(response: Response, limit: int = Query(None), offset: int = Query(None), order: str = Query("id")):
+    order_dict = {
+        "id": "EmployeeID",
+        "first_name": "FirstName",
+        "last_name": "LastName",
+        "city": "City",
+    }
+    offset_sql = ""
+    limit_sql = ""
+    order_sql = ""
+    if offset:
+        offset_sql +=f" OFFSET {offset}"
+    if limit:
+        limit_sql +=f" LIMIT {limit}"
+    try:
+        order_sql = f" ORDER BY {order_dict[order]}"
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Wrong order param!",
+        )
+    response.status_code = status.HTTP_200_OK
+    dbrouter.db_connection.row_factory = sqlite3.Row
+    employees = dbrouter.db_connection.execute("SELECT EmployeeID, LastName, FirstName, City FROM Employees" + order_sql + limit_sql + offset_sql).fetchall()
+    employees = [{"id": x["EmployeeID"], "last_name": x["LastName"], "first_name": x["FirstName"], "city": x["City"]} for x in employees]
+    return {"employees": employees}
+
+
+@dbrouter.get("/products_extended")
+async def get_products_ext(response: Response):
+    response.status_code = status.HTTP_200_OK
+    dbrouter.db_connection.row_factory = sqlite3.Row
+    prod_ext = dbrouter.db_connection.execute("""
+    SELECT ProductID, ProductName, CategoryName, CompanyName
+    FROM Products p 
+    JOIN Suppliers s USING (SupplierID)
+    JOIN Categories c USING (CategoryID);
+    """)
+    prod_ext = [{"id": x["ProductID"], "name": x["ProductName"], "category": x["CategoryName"], "supplier": x["CompanyName"]} for x in prod_ext]
+    return {"products_extended": prod_ext}
+
+
+@dbrouter.get("/products/{product_id}/orders")
+async def get_products_orders(response: Response, product_id: int):
+    dbrouter.db_connection.row_factory = sqlite3.Row
+    ord_id_check = dbrouter.db_connection.execute(f"SELECT ProductID from Products WHERE ProductID = {product_id}")
+    if ord_id_check is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Product with such id doesnt exist!",
+        )
+    response.status_code = status.HTTP_200_OK
+    prod_ord = dbrouter.db_connection.execute("""
+    SELECT o.OrderID as OrderID, CompanyName, Quantity, ROUND((UnitPrice * Quantity) - (Discount * (UnitPrice * Quantity)),2) as TotalPrice
+    FROM Orders o 
+    JOIN Customers c USING (CustomerID)
+    JOIN "Order Details" od WHERE (od.OrderID = o.OrderID AND od.ProductID = ?) """, (product_id,))
+    prod_ord = [{"id": x["OrderID"], "customer": x["CompanyName"], "quantity": x["Quantity"], "total_price": x["TotalPrice"]} for x in prod_ord]
+    return {"orders": prod_ord}
+
+
+@dbrouter.post("/categories")
+async def post_categories(response: Response, category: CategoryInput):
+    cursor = dbrouter.db_connection.execute(
+        f"INSERT INTO Categories (CategoryName) VALUES ('{category.name}')")
+    dbrouter.db_connection.commit()
+    response.status_code = status.HTTP_201_CREATED
+    return CategoryOutput(
+        id = cursor.lastrowid,
+        name = category.name
+    )
+
+
+async def check_category_id(category_id):
+    dbrouter.db_connection.row_factory = sqlite3.Row
+    cat_check = dbrouter.db_connection.execute(f"SELECT CategoryID from Categories WHERE CategoryID = {category_id}")
+    if cat_check is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Category with such id doesnt exist!",
+        )
+
+
+@dbrouter.put("/categories/{category_id}")
+async def put_categories(response: Response, category: CategoryInput, category_id: int):
+    check_category_id(category_id)
+    response.status_code = status.HTTP_200_OK
+    dbrouter.db_connection.execute(
+        "UPDATE Categories SET CategoryName = ? WHERE CategoryID = ?", (
+        category.name, category_id)
+    )
+    dbrouter.db_connection.commit()
+    return CategoryOutput(
+        id = category_id,
+        name = category.name
+    )
+
+
+@dbrouter.delete("/categories/{category_id}")
+async def del_categories(response: Response, category: CategoryInput, category_id: int):
+    check_category_id(category_id)
+    response.status_code = status.HTTP_200_OK
+    dbrouter.db_connection.execute(
+        "DELETE FROM Categories WHERE CategoryID = ?",
+        (category_id, )
+    )
+    dbrouter.db_connection.commit()
+    return {"deleted": 1}
