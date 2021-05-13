cd045e5000295041eb1c326c9f0ac81faa62e6fc
lstanisz <lukasz.staniszewski.stud@pw.edu.pl>
2021-05-13 19:39:34 +0200

    na paryskie getto pada deszcz ale nie chcesz o tym slyszec

--- dblib/schemas.py
+++ dblib/schemas.py
@@ -24,16 +24,16 @@ class Supplier(BaseModel):
 class SupplierFull(BaseModel):
     SupplierID: PositiveInt
     CompanyName: constr(max_length=40)
-    ContactName: constr(max_length=30)
-    ContactTitle: constr(max_length=30)
-    Address: constr(max_length=60)
-    City: constr(max_length=15)
-    Region: NullableStr
-    PostalCode: constr(max_length=10)
-    Country: constr(max_length=15)
-    Phone: constr(max_length=24)
-    Fax: NullableStr
-    HomePage: NullableStr
+    ContactName: Optional[constr(max_length=30)]
+    ContactTitle: Optional[constr(max_length=30)]
+    Address: Optional[constr(max_length=60)]
+    City: Optional[constr(max_length=15)]
+    Region: Optional[constr(max_length=15)]
+    PostalCode: Optional[constr(max_length=10)]
+    Country: Optional[constr(max_length=15)]
+    Phone: Optional[constr(max_length=24)]
+    Fax: Optional[constr(max_length=24)]
+    HomePage: Optional[str]
 
     class Config:
         orm_mode = True
